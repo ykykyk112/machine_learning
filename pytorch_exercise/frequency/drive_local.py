a91dce2d95d97aed1a55c1e98a6660f3c1c0fed1
ykykyk112 <ykykyk112@naver.com>
2021-05-27 19:59:34 +0900

    2021.05.27 * Change augmentaion (no random crop)

--- pytorch_exercise/frequency/drive_local.py
+++ pytorch_exercise/frequency/drive_local.py
@@ -46,7 +46,7 @@ def drive():
     train_transform = transforms.Compose([
         transforms.Resize(64),
         transforms.RandomHorizontalFlip(),
-        transforms.RandomCrop(size=64, padding=4),
+        #transforms.RandomCrop(size=64, padding=4),
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
