32429386663eaa401337b915905e0703de11d59b
ykykyk112 <ykykyk112@naver.com>
2021-06-01 12:15:31 +0900

    2021.06.01 * Get dataloader in server

--- pytorch_exercise/frequency/drive_local.py
+++ pytorch_exercise/frequency/drive_local.py
@@ -67,21 +67,17 @@ def drive():
 
     #train_save_model.train_eval_model_gpu(recover_model, 1, device, train_loader, test_loader, False, None)
 
-    train_image, test_image = torch.empty((50000, 3, 224, 224)), torch.empty((10000, 3, 224, 224))
-    train_label, test_label = torch.empty((50000,)), torch.empty((10000,))
+    test_image = torch.empty((10000, 3, 224, 224))
+    test_label = torch.empty((10000,))
     
-    for idx, (train_data, train_target) in enumerate(train_loader) :
-        train_image[idx*50:(idx+1)*50] = train_data
-        train_label[idx*50:(idx+1)*50] = train_target
 
     for idx, (test_data, test_target) in enumerate(test_loader) :
-        test_image[idx*50:(idx+1)*50] = test_data
-        test_label[idx*50:(idx+1)*50] = test_target
+        test_image[idx*50:(idx+1)*50] = test_data.data
+        test_label[idx*50:(idx+1)*50] = test_target.data
 
-    np.save('./train_image.npy', train_image.detach())
-    np.save('./train_label.npy', train_label.detach())
-    np.save('./test_image.npy', test_image.detach())
-    np.save('./test_label.npy', test_label.detach())
+
+    np.save('./test_image.npy', test_image.data)
+    np.save('./test_label.npy', test_label.data)
 
     print('save all complete')
 
