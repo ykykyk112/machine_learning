56d39ddbdd36cde401cda6e17c0c64387ddd8474
ykykyk112 <ykykyk112@naver.com>
2021-06-03 13:13:08 +0900

    2021.06.03 * Test Parrarell network using STL10 dataset

--- pytorch_exercise/frequency/parallel.py
+++ pytorch_exercise/frequency/parallel.py
@@ -29,7 +29,7 @@ class parallel_net(nn.Module):
 
         # register forward & backward hook on last nn.Conv2d module of recover_gradcam
         for m in reversed(list(self.recover_gradcam.modules())):
-            if isinstance(m, nn.Conv2d):
+            if isinstance(m, RecoverConv2d):
                 m.register_forward_hook(self.forward_hook)
                 m.register_full_backward_hook(self.backward_hook)
                 print(m)
