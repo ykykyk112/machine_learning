27d44055d47775a8919756810ded24ff18e58569
ykykyk112 <ykykyk112@naver.com>
2021-06-03 18:08:14 +0900

    2021.06.03 * Change layer which is applied hook * RecoverConv2d -> Conv2d

--- pytorch_exercise/frequency/vgg_gradcam.py
+++ pytorch_exercise/frequency/vgg_gradcam.py
@@ -28,8 +28,8 @@ class recovered_net(nn.Module):
         
         self.optimizer = optim.SGD(self.parameters(), lr = 1e-2, momentum = 0.9, weight_decay=0.0015)
         self.loss = nn.CrossEntropyLoss()
-        #self.scheduler = StepLR(self.optimizer, step_size=30, gamma=0.1)
-        self.scheduler = ReduceLROnPlateau(self.optimizer, 'min', 0.1, 5)
+        self.scheduler = StepLR(self.optimizer, step_size=30, gamma=0.1)
+        #self.scheduler = ReduceLROnPlateau(self.optimizer, 'min', 0.1, 5)
 
 
     def _make_layer_conv(self, conv_layers, recover_mode, upsample_mode):
