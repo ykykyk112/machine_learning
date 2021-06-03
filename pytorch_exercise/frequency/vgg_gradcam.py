7656be4b28d6516eec2383d573254c74a229c787
ykykyk112 <ykykyk112@naver.com>
2021-06-03 23:40:38 +0900

    2021.06.03

--- pytorch_exercise/frequency/vgg_gradcam.py
+++ pytorch_exercise/frequency/vgg_gradcam.py
@@ -28,7 +28,7 @@ class recovered_net(nn.Module):
         
         self.optimizer = optim.SGD(self.parameters(), lr = 1e-2, momentum = 0.9, weight_decay=0.0015)
         self.loss = nn.CrossEntropyLoss()
-        self.scheduler = StepLR(self.optimizer, step_size=30, gamma=0.1)
+        self.scheduler = StepLR(self.optimizer, step_size=25, gamma=0.1)
         #self.scheduler = ReduceLROnPlateau(self.optimizer, 'min', 0.1, 5)
 
 
