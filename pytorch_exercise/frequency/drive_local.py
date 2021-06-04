e182463ff94dfb4c8fb29f3f030aaf651708250a
ykykyk112 <ykykyk112@naver.com>
2021-06-04 12:31:02 +0900

    2021.06.04 * Change lr_scheduler and training epoch

--- pytorch_exercise/frequency/drive_local.py
+++ pytorch_exercise/frequency/drive_local.py
@@ -68,7 +68,7 @@ def drive():
     test_loader = DataLoader(test_set, batch_size = 50, shuffle = False, num_workers=2)
 
 
-    train_save_model.train_eval_model_gpu(recover_model, 90, device, train_loader, test_loader, False, None)
+    train_save_model.train_eval_model_gpu(recover_model, 36, device, train_loader, test_loader, False, None)
 
 
 
