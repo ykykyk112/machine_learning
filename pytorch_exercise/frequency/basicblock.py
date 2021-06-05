d828d818e7f4eb1f7c1b6b43cd41c24e91be6b15
ykykyk112 <ykykyk112@naver.com>
2021-06-05 10:32:50 +0900

    2021.06.05 * Separating Conv2d layer in basicblock

--- pytorch_exercise/frequency/basicblock.py
+++ pytorch_exercise/frequency/basicblock.py
@@ -12,7 +12,7 @@ class RecoverConv2d(nn.Module):
         self.comp_mode = comp_mode
         self.upsample_mode = upsample_mode
         #self.sum_factor = torch.nn.Parameter(torch.ones((1, self.out_channels, 1, 1))*(0.), requires_grad = True)
-        self.sum_factor = torch.nn.Parameter(torch.tensor([0.]), requires_grad = True)
+        self.sum_factor = torch.nn.Parameter(torch.tensor([1.0]), requires_grad = True)
         #self.comp_conv = nn.Conv2d(self.in_channels, self.out_channels, 1, 1, 0)
 
         self.pooling_kernel_size = 2
@@ -71,7 +71,7 @@ class RecoverConv2d(nn.Module):
         #     ret_second_forward = self.feed_forward(torch.abs(ret_substract))
 
         ret_second_forward = self.second_forward(torch.abs(ret_substract))
-        
+
         ret_second_forward = self.second_batch_relu(ret_second_forward)
         ret_second_forward = self.second_max_pooling(ret_second_forward)
         
