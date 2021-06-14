05fba4a185171b36d4665098981aef2209fe4f12
ykykyk112 <ykykyk112@naver.com>
2021-06-14 20:04:33 +0900

    2021.06.14 * Test code

--- pytorch_exercise/cnn_cifar10/model/train_save_model.py
+++ pytorch_exercise/cnn_cifar10/model/train_save_model.py
@@ -99,6 +99,7 @@ def train_eval_model_gpu(model, epoch, device, train_loader, test_loader, cam_mo
     best_valid_loss = 100.0
     best_valid_acc = 0.0
     converge_count = 0
+    best_epoch = 0
 
     
     for i in range(epoch) :
@@ -118,8 +119,8 @@ def train_eval_model_gpu(model, epoch, device, train_loader, test_loader, cam_mo
             if cam_mode :
                 train_output, _ = model.forward(train_data)
             else :
-                train_output = model(train_data)
-                #train_output = model(train_data, train_target, idx)
+                #train_output = model(train_data)
+                train_output = model(train_data, train_target, idx)
 
             t_loss = model.loss(train_output, train_target)
             t_loss.backward()
@@ -132,30 +133,30 @@ def train_eval_model_gpu(model, epoch, device, train_loader, test_loader, cam_mo
             train_acc += torch.sum(pred == train_target.data)
             
 
-        with torch.no_grad():
+        #with torch.no_grad():
 
-            model.eval()
-            
-            for idx, (valid_data, valid_target) in enumerate(test_loader):
-                
-                valid_data, valid_target = valid_data.to(device), valid_target.to(device)
-
-                model.optimizer.zero_grad()
-
-                if cam_mode :
-                    valid_output, _ = model(valid_data)
-                else :
-                    valid_output = model(valid_data)
-                    #valid_output = model(valid_data, valid_target, idx, True)
-
-
-                v_loss = model.loss(valid_output, valid_target)
-                #print(v_loss.item())
-                _, v_pred = torch.max(valid_output, dim = 1)
+        model.eval()
         
+        for idx, (valid_data, valid_target) in enumerate(test_loader):
+            
+            valid_data, valid_target = valid_data.to(device), valid_target.to(device)
 
-                valid_loss += v_loss.item()
-                valid_acc += torch.sum(v_pred == valid_target.data)
+            model.optimizer.zero_grad()
+
+            if cam_mode :
+                valid_output, _ = model(valid_data)
+            else :
+                #valid_output = model(valid_data)
+                valid_output = model(valid_data, valid_target, idx, True)
+
+
+            v_loss = model.loss(valid_output, valid_target)
+            #print(v_loss.item())
+            _, v_pred = torch.max(valid_output, dim = 1)
+    
+
+            valid_loss += v_loss.item()
+            valid_acc += torch.sum(v_pred == valid_target.data)
 
 
         train_acc = train_acc*(100.)
@@ -201,13 +202,18 @@ def train_eval_model_gpu(model, epoch, device, train_loader, test_loader, cam_mo
             print('epoch.{0:3d} \t train_ls : {1:.6f} \t train_ac : {2:.4f}% \t valid_ls : {3:.6f} \t valid_ac : {4:.4f}% \t lr : {5:.5f} 1st : {6:.4f} 2nd : {7:.4f}'.format(i+1, avg_train_loss, avg_train_acc, avg_valid_loss, avg_valid_acc, curr_lr, first_weight, second_weight))        
         
         if avg_valid_acc > best_valid_acc : best_valid_acc = avg_valid_acc
-        if avg_valid_loss < best_valid_loss : best_valid_loss = avg_valid_loss
+        if avg_valid_loss < best_valid_loss : 
+            best_valid_loss = avg_valid_loss
+            best_loss_parameter = model.state_dict()
+            best_latest_valid_cam = model.latest_valid_cam.detach()
+            best_epoch = i+1
         
-    #np.save('./ImageNet/cam_ret_imagenet_subset_sum.npy', model.latest_valid_cam.detach().cpu())
-    model = model.to('cpu')
-    torch.save(model.state_dict(), './ImageNet/baseline_imagenet_subset2.pth')
+    #np.save('./ImageNet/cam_ret_imagenet_subset_48.npy', best_latest_valid_cam.cpu())
+    best_loss_parameter = best_loss_parameter.to('cpu')
+    #torch.save(best_loss_parameter, './ImageNet/target_imagenet_subset_48.pth')
+    torch.save(best_loss_parameter, './ImageNet/test.pth')
 
-    print('model parameter, grad cam heatmap are saved')
+    print('model parameter, grad cam heatmap are saved, best epoch :', best_epoch)
     print('best loss : {0:.6f}, base acc : {1:.4f}'.format(best_valid_loss, best_valid_acc))
 
     return
