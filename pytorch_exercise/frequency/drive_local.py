2e06105d21d40ccccd274dfd84310c9f3ddd429c
ykykyk112 <ykykyk112@naver.com>
2021-06-16 23:44:11 +0900

    2021.06.16 * Save best validation loss's parameter

--- pytorch_exercise/frequency/drive_local.py
+++ pytorch_exercise/frequency/drive_local.py
@@ -35,7 +35,7 @@ def drive():
     device = torch.device(1)
 
     #print('target(0.0), 224x224 STL10, random seed : 42, cam-layer : first MaxPool2d and RecoverConv2d')
-    print('baseline model, ImageNet subset (55 classes, train image : 71159, test_image : 2750)')
+    print('baseline model, ImageNet subset (30 classes, train image : 39000, test_image : 1500)')
     if not True:
         print('Run baseline model...')
         recover_model = recovered_net(baseline_layers, 'W', True).to(device)
@@ -65,8 +65,8 @@ def drive():
         transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225]),
     ])
 
-    train_set = torchvision.datasets.ImageFolder(root = './data/mini_imagenet/train_subset_sum', transform=train_transform)
-    test_set = torchvision.datasets.ImageFolder(root = './data/mini_imagenet/val_subset_sum', transform=test_transform)
+    train_set = torchvision.datasets.ImageFolder(root = './data/mini_imagenet/train', transform=train_transform)
+    test_set = torchvision.datasets.ImageFolder(root = './data/mini_imagenet/val', transform=test_transform)
 
     train_loader = DataLoader(train_set, batch_size = 32, shuffle = True, num_workers=2)
     test_loader = DataLoader(test_set, batch_size = 32, shuffle = False, num_workers=2)
--- pytorch_exercise/frequency/parallel.py
+++ pytorch_exercise/frequency/parallel.py
@@ -30,8 +30,8 @@ class parallel_net(nn.Module):
             self.device = device
  
 
-        self.latest_train_cam = torch.ones((71159, 1, 7, 7), dtype=torch.float32, requires_grad=False).to(self.device)
-        self.latest_valid_cam = torch.ones((2750, 1, 7, 7), dtype=torch.float32, requires_grad=False).to(self.device)
+        self.latest_train_cam = torch.ones((39000, 1, 7, 7), dtype=torch.float32, requires_grad=False).to(self.device)
+        self.latest_valid_cam = torch.ones((1500, 1, 7, 7), dtype=torch.float32, requires_grad=False).to(self.device)
 
         # register forward & backward hook on last nn.Conv2d module of recover_gradcam
         #for m in reversed(list(self.recover_gradcam.modules())):
@@ -58,11 +58,11 @@ class parallel_net(nn.Module):
 
         # 50 is batch-size
         if not eval:
-            if b_end > 71159 : b_end = 71159
+            if b_end > 39000 : b_end = 39000
             latest_heatmap = self.latest_train_cam[b_start:b_end]
             latest_heatamp = latest_heatmap
         else :
-            if b_end > 2750 : b_end = 2750
+            if b_end > 1500 : b_end = 1500
             latest_heatmap = self.latest_valid_cam[b_start:b_end]
             latest_heatamp = latest_heatmap
 
@@ -90,10 +90,10 @@ class parallel_net(nn.Module):
         
 
         if not eval:
-            if b_end > 71159 : b_end = 71159
+            if b_end > 39000 : b_end = 39000
             self.latest_train_cam[b_start:b_end] = cam_rescaled
         else :
-            if b_end > 2750 : b_end = 2750
+            if b_end > 1500 : b_end = 1500
             self.latest_valid_cam[b_start:b_end] = cam_rescaled
 
         return cam_rescaled
