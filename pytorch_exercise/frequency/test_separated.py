1c1e8fc6e58859ea575ce1454ae33e60dd3979a8
ykykyk112 <ykykyk112@naver.com>
2021-06-17 16:30:03 +0900

    2021.06.17 * Run Separated Network

--- pytorch_exercise/frequency/test_separated.py
+++ pytorch_exercise/frequency/test_separated.py
@@ -50,7 +50,7 @@ def drive():
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
 
-    train_set = torchvision.datasets.CIFAR10(root = './data', dowmload = True, transform=train_transform)
+    train_set = torchvision.datasets.CIFAR10(root = './data', download = True, transform=train_transform)
     test_set = torchvision.datasets.CIFAR10(root = './data', download = True, transform=test_transform)
 
     train_loader = DataLoader(train_set, batch_size = 32, shuffle = True, num_workers=2)
