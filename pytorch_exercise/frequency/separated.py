b05bf1108f74c47d6e35494863f07c31f6377e54
ykykyk112 <ykykyk112@naver.com>
2021-06-18 01:22:42 +0900

    2021.06.17 * Run Separated Network

--- pytorch_exercise/frequency/separated.py
+++ pytorch_exercise/frequency/separated.py
@@ -33,11 +33,11 @@ class separated_network(nn.Module):
             nn.Linear(512, 10)
         )
         self.boundary_classifier = nn.Sequential(
+            nn.Dropout2d(0.5),
             nn.Linear(2 * 2 * 512, 1024),
-            nn.Dropout2d(0.5),
             nn.ReLU(inplace=True),
-            nn.Linear(1024, 512),
             nn.Dropout2d(0.5),
+            nn.Linear(1024, 512),
             nn.ReLU(inplace=True),
             nn.Linear(512, 10)
         )
