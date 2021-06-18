ec3644fc388f8a23854baba7351f9af293fb4bb1
ykykyk112 <ykykyk112@naver.com>
2021-06-18 16:27:56 +0900

    2021.06.17 * Run Separated Network

--- pytorch_exercise/cnn_cifar10/model/train_save_model.py
+++ pytorch_exercise/cnn_cifar10/model/train_save_model.py
@@ -325,10 +325,10 @@ def train_eval_model_gpu_cam(model, epoch, device, train_loader, test_loader, ca
         avg_valid_loss = valid_loss/len(test_loader)
         valid_loss_history.append(float(avg_valid_loss))
 
-        avg_train_acc = train_acc/39000.
+        avg_train_acc = train_acc/50000.
         train_acc_history.append(float(avg_train_acc))
 
-        avg_valid_acc = valid_acc/1500.
+        avg_valid_acc = valid_acc/10000.
         valid_acc_history.append(float(avg_valid_acc))
 
         # Code about early_stopping
