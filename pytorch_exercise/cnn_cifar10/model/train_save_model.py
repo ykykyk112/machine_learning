c6e4a6fe41d60dc2f09a794cea7a4b2a913f44a2
ykykyk112 <ykykyk112@naver.com>
2021-06-20 23:12:34 +0900

    2021.06.18 * Run CAM masking model

--- pytorch_exercise/cnn_cifar10/model/train_save_model.py
+++ pytorch_exercise/cnn_cifar10/model/train_save_model.py
@@ -286,8 +286,8 @@ def train_eval_model_gpu_cam(model, epoch, device, train_loader, test_loader, ca
             if cam_mode :
                 train_output, _ = model.forward(train_data)
             else :
-                #train_output = model(train_data)
-                train_output = model(train_data, train_target, idx)
+                train_output = model(train_data)
+                #train_output = model(train_data, train_target, idx)
 
             t_loss = model.loss(train_output, train_target)
             t_loss.backward()
@@ -301,29 +301,29 @@ def train_eval_model_gpu_cam(model, epoch, device, train_loader, test_loader, ca
             train_acc += torch.sum(pred == train_target.data)
             
 
-        #with torch.no_grad():
+        with torch.no_grad():
 
-        model.eval()
-        
-        for idx, (valid_data, valid_target) in enumerate(test_loader):
+            model.eval()
             
-            valid_data, valid_target = valid_data.to(device), valid_target.to(device)
+            for idx, (valid_data, valid_target) in enumerate(test_loader):
+                
+                valid_data, valid_target = valid_data.to(device), valid_target.to(device)
 
-            model.optimizer.zero_grad()
+                model.optimizer.zero_grad()
 
-            if cam_mode :
-                valid_output, _ = model(valid_data)
-            else :
-                #valid_output = model(valid_data)
-                valid_output = model(valid_data, valid_target, idx, True)
+                if cam_mode :
+                    valid_output, _ = model(valid_data)
+                else :
+                    valid_output = model(valid_data)
+                    #valid_output = model(valid_data, valid_target, idx, True)
 
 
-            v_loss = model.loss(valid_output, valid_target)
-            #print(v_loss.item())
-            _, v_pred = torch.max(valid_output, dim = 1)
+                v_loss = model.loss(valid_output, valid_target)
+                #print(v_loss.item())
+                _, v_pred = torch.max(valid_output, dim = 1)
 
-            valid_loss += v_loss.item()
-            valid_acc += torch.sum(v_pred == valid_target.data)
+                valid_loss += v_loss.item()
+                valid_acc += torch.sum(v_pred == valid_target.data)
 
 
         train_acc = train_acc*(100.)
