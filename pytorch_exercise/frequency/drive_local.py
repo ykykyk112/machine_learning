c4e6d07a5c63368a36d6d193019ae09c90e8a846
ykykyk112 <ykykyk112@naver.com>
2021-06-21 10:13:59 +0900

    2021.06.21 * Run baseline model on combinated dataset

--- pytorch_exercise/frequency/drive_local.py
+++ pytorch_exercise/frequency/drive_local.py
@@ -34,8 +34,8 @@ def drive():
     device = torch.device(1)
 
     #print('target(0.0), 224x224 STL10, random seed : 42, cam-layer : first MaxPool2d and RecoverConv2d')
-    print('target model channel-wise weight, ImageNet subset (55 classes, train image : 71159, test_image : 2750)')
-    if not True:
+    print('baseline model, ImageNet subset (55 classes, train image : 71159, test_image : 2750)')
+    if not False:
         print('Run baseline model...')
         recover_model = recovered_net(baseline_layers, 'W', True).to(device)
         #recover_model = AlexNet(True, 'W', True).to(device)
