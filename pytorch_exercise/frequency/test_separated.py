131b0c300eae9dcc3fb81b9a61c95ee7bc2bc8d3
ykykyk112 <ykykyk112@naver.com>
2021-06-23 13:59:30 +0900

    2021.06.22 * Change weighted sum option

--- pytorch_exercise/frequency/test_separated.py
+++ pytorch_exercise/frequency/test_separated.py
@@ -30,7 +30,7 @@ def drive():
     boundary_layers = [64, 128, 256, 512]
 
     baseline_layers = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M']
-    device = torch.device(2)
+    device = torch.device(1)
 
     #print('target(0.0), 224x224 STL10, random seed : 42, cam-layer : first MaxPool2d and RecoverConv2d')
     if not True:
@@ -45,18 +45,20 @@ def drive():
     train_transform = transforms.Compose([
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        #transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        transforms.Normalize((0.4467, 0.4398, 0.4066), (0.2241, 0.2214, 0.2238)),
         #AddGaussianNoise(0., 1., 0.3),
     ])
 
     test_transform = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        #transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        transforms.Normalize((0.4467, 0.4398, 0.4066), (0.2241, 0.2214, 0.2238)),
         #AddGaussianNoise(0., 1., 0.3),
     ])
 
-    train_set = torchvision.datasets.CIFAR10(root = './data', train = True, download = True, transform=train_transform)
-    test_set = torchvision.datasets.CIFAR10(root = './data', train = False, download = True, transform=test_transform)
+    train_set = torchvision.datasets.STL10(root = './data', split = 'train', download = True, transform=train_transform)
+    test_set = torchvision.datasets.STL10(root = './data', split = 'test', download = True, transform=test_transform)
 
     train_loader = DataLoader(train_set, batch_size = 32, shuffle = True, num_workers=2)
     test_loader = DataLoader(test_set, batch_size = 32, shuffle = False, num_workers=2)
