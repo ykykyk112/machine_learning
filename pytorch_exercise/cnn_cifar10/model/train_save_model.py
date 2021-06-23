92526b23a0a2fdbd28375d3e3557eb5f2b05110e
ykykyk112 <ykykyk112@naver.com>
2021-06-23 14:06:17 +0900

    2021.06.22 * Change weighted sum option

--- pytorch_exercise/cnn_cifar10/model/train_save_model.py
+++ pytorch_exercise/cnn_cifar10/model/train_save_model.py
@@ -240,7 +240,7 @@ def train_eval_model_gpu(model, epoch, device, train_loader, test_loader, cam_mo
     # torch.save(best_loss_parameter, './ImageNet/target_imagenet_subset_color.pth')
 
     # print('model parameter, grad cam heatmap are saved, best epoch :', best_epoch)
-    print('best acc : {0:.4f}%, best boundary acc : {1:.4f}%, best ensemble acc : {2:.4f}%'.format(best_valid_acc/10000., best_boundary_valid_acc/10000., best_ensemble_valid_acc/10000.))
+    print('best acc : {0:.4f}%, best boundary acc : {1:.4f}%, best ensemble acc : {2:.4f}%'.format(best_valid_acc/8000., best_boundary_valid_acc/8000., best_ensemble_valid_acc/8000.))
 
     return
 
