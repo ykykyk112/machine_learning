f98d5ee233b3e8034ef185fd6ec9eb9cdd930235
ykykyk112 <ykykyk112@naver.com>
2021-06-23 14:03:42 +0900

    2021.06.22 * Change weighted sum option

--- pytorch_exercise/cnn_cifar10/model/train_save_model.py
+++ pytorch_exercise/cnn_cifar10/model/train_save_model.py
@@ -192,14 +192,14 @@ def train_eval_model_gpu(model, epoch, device, train_loader, test_loader, cam_mo
         avg_valid_loss = valid_loss/len(test_loader)
         valid_loss_history.append(float(avg_valid_loss))
 
-        avg_train_acc = train_acc/50000.
-        avg_boundary_train_acc = boundary_acc/50000.
-        avg_ensemble_train_acc = ensemble_acc/50000.
+        avg_train_acc = train_acc/5000.
+        avg_boundary_train_acc = boundary_acc/5000.
+        avg_ensemble_train_acc = ensemble_acc/5000.
         train_acc_history.append(float(avg_train_acc))
 
-        avg_valid_acc = valid_acc/10000.
-        avg_boundary_valid_acc = valid_boundary_acc/10000.
-        avg_ensemble_valid_acc = valid_ensemble_acc/10000.
+        avg_valid_acc = valid_acc/8000.
+        avg_boundary_valid_acc = valid_boundary_acc/8000.
+        avg_ensemble_valid_acc = valid_ensemble_acc/8000.
         valid_acc_history.append(float(avg_valid_acc))
 
         # Code about early_stopping
