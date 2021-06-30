ed3b10149756f0e203acde8b77b9d37aaf17dfa1
ykykyk112 <ykykyk112@naver.com>
2021-06-30 14:19:41 +0900

    2021.06.30 * Convert python list to torch.nn.ModuleList * Detach boundary map on boundary network

--- pytorch_exercise/frequency/drive_local.py
+++ pytorch_exercise/frequency/drive_local.py
@@ -36,7 +36,7 @@ def drive():
     device = torch.device(1)
 
     #print('target(0.0), 224x224 STL10, random seed : 42, cam-layer : first MaxPool2d and RecoverConv2d')
-    print('target model, no sum factor, no inception module, max-pooling, ImageNet subset (55 classes, train image : 71159, test_image : 2750)')
+    print('target model, no sum factor, no inception module, max-pooling, 0.5-weight on boundary & ensemble ImageNet subset (55 classes, train image : 71159, test_image : 2750)')
     if not True:
         print('Run baseline model...')
         recover_model = recovered_net(baseline_layers, 'W', True).to(device)
