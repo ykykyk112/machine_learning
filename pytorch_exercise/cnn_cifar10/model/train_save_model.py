3d4af1d4d548299f0348f16148966abf637634ea
ykykyk112 <ykykyk112@naver.com>
2021-07-04 12:26:42 +0900

    2021.07.04 * Add ReLU module before Concat feature

--- pytorch_exercise/cnn_cifar10/model/train_save_model.py
+++ pytorch_exercise/cnn_cifar10/model/train_save_model.py
@@ -243,8 +243,8 @@ def train_eval_model_gpu(model, epoch, device, train_loader, test_loader, cam_mo
         
     # np.save('./ImageNet/cam_ret_imagenet_subset_color.npy', best_latest_valid_cam.cpu())
     # #torch.save(best_loss_parameter, './ImageNet/target_imagenet_subset_48.pth')
-    torch.save(best_boundary_loss_parameter, './ImageNet/ImageNet_sum_210701/separated_boundary_relu_fmapconcat_1024.pth')
-    torch.save(best_loss_parameter, './ImageNet/ImageNet_sum_210701/separated_ensemble_relu_fmapconcat_1024.pth')
+    torch.save(best_boundary_loss_parameter, './ImageNet/ImageNet_sum_210701/separated_boundary_relu_fmapconcat_2048.pth')
+    torch.save(best_loss_parameter, './ImageNet/ImageNet_sum_210701/separated_ensemble_relu_fmapconcat_2048.pth')
 
     # print('model parameter, grad cam heatmap are saved, best epoch :', best_epoch)
     print('best acc : {0:.4f}%, best boundary acc : {1:.4f}%, best ensemble acc : {2:.4f}%'.format(best_valid_acc/n_valid, best_boundary_valid_acc/n_valid, best_ensemble_valid_acc/n_valid))
